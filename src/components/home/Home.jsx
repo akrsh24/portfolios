a3a30e1b52ad775d085188fb56503f94ff75142b
akasriva2 <akasriva2@publicisgroupe.net>
2020-06-19 20:00:16 +0530

    Added some images size changes for mobile screen

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
         <div className="home-container">
             <div className="intro-container">
                 <div className="img-container">
-                    <img className="img-item" src="/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" />
+                    <img className="img-item" src="/portfolio/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" />
+                    {/* <img className="img-item" src="/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" /> */}
                 </div>
             </div>
         </div>

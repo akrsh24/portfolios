b10c346270366144bb1562d0c24e1802038ccd51
akasriva2 <akasriva2@publicisgroupe.net>
2020-06-19 23:56:57 +0530

    Added intro animation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,9 +6,11 @@ const Home = () => {
         <div className="home-container">
             <div className="intro-container">
                 <div className="img-container">
-                    <img className="img-item" src="/portfolio/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" />
+                    {/* <img className="img-item" src="/portfolio/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" /> */}
                     {/* <img className="img-item" src="/images/personal_dp.jpeg" alt="akarsh.jpeg" loading="lazy" /> */}
-                </div>
+                    <h4 className="intro-line-1">Hi!</h4>
+                    <h4 className="intro-line-1">I am Akarsh</h4>
+                    <p className="intro-line-1">I am a <span id="spin"></span>!</p>                </div>
             </div>
         </div>
     );
--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -4,21 +4,22 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div class="nav-container">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <NavLink to='/portfolio/' class="navbar-brand">
-                    <span class="navbar-brand mb-0 h1">Akarsh</span>
+        <div className="nav-container">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <NavLink to='/portfolio/' className="navbar-brand">
+                    <span className="navbar-brand mb-0 h1">Akarsh</span>
                 </NavLink>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <NavLink to='/portfolio/' class="nav-link">Home</NavLink>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to='/portfolio/' className="nav-link">Home</NavLink>
                         </li>
-                        <li class="nav-item">
-                            <NavLink to='/portfolio/about' class="nav-link">About</NavLink>
+                        <li className="nav-item">
+                            <NavLink to='/portfolio/about' className="nav-link">About</NavLink>
                         </li>
                     </ul>
                 </div>

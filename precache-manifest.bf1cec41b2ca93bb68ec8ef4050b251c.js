1c85c7242a733df7a5112c574625141ffa6554ff
akasriva2 <akasriva2@publicisgroupe.net>
2020-06-19 19:09:18 +0530

    Updates

---
+++ precache-manifest.bf1cec41b2ca93bb68ec8ef4050b251c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e781609464f7a9f110c3d372a92648c6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ed6eda8ce4fd00290ba",
+    "url": "/portfolio/static/css/main.50fa786d.chunk.css"
+  },
+  {
+    "revision": "b3be72e2247cafc71b24",
+    "url": "/portfolio/static/js/2.6dbdf959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.6dbdf959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed6eda8ce4fd00290ba",
+    "url": "/portfolio/static/js/main.0e4fbe5e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
